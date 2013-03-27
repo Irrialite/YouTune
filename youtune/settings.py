d18e1457ba8e0f9bf15e02d7325324ce32e7f2c3
testUser99 <nomail@none.com>
2013-03-27 18:52:36 +0100

    Database name reafactor
    no absolute path, just name.db

--- youtune/settings.py
+++ youtune/settings.py
@@ -13,7 +13,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',# sqlite3 zato ker generira samo 1 file, na pc  | # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'C:\Users\SnowMan\youtune\mojaBaza.db',                      # Or path to database file if using sqlite3.
+        'NAME': 'maindba.db',                      # Or path to database file if using sqlite3.
 
         # The following settings are not used with sqlite3:
         'USER': '',

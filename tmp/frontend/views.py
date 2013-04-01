d7ddae8026191dd30ee62bf43697602307ce4f54
Irrialite <crt.kristl@gmail.com>
2013-04-01 22:20:56 +0200

    Cleaned up a little.

--- tmp/frontend/views.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-from django.views import generic as generic_views
-
-class HomeView( generic_views.TemplateView ):
-	template_name = 'home.html'
-        
-class SearchView( generic_views.TemplateView ):
-	template_name = 'search.html'

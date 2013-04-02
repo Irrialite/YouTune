b4e967b5231a4f9bfd6797ebd947d5555d9dfebe
Primož Jeras <normandyh@gmail.com>
2013-04-02 03:50:41 +0200

    Readd.

---
+++ youtune/polls/admin.py
@@ -0,0 +1,28 @@
+__author__ = 'SnowMan'
+
+
+from django.contrib import admin
+
+from youtune.polls.models import Poll, Choice
+
+
+#this change makes the Publication date come before the Question field - says so in tutorial
+#plus other stuff related to Polls
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+        ]
+    inlines = [ChoiceInline]
+    list_display = ('question', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+
+    search_fields = ['question']
+    date_hierarchy = 'pub_date'
+
+
+admin.site.register(Poll, PollAdmin)
